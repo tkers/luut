6d0d0f588c9ab7996bbbef972580b069459d2582
Tijn Kersjes <tijn@divbyzero.nl>
2021-03-11 15:15:40 +0100

    Make canvas width responsive

--- src/canvas.js
+++ src/canvas.js
@@ -16,8 +16,9 @@ export const resizeCanvas = function (canvas, width, height, scale = 1) {
   const ratio = getPixelRatio(ctx);
   canvas.width = width * ratio;
   canvas.height = height * ratio;
-  canvas.style.width = `${width * scale}px`;
-  canvas.style.height = `${height * scale}px`;
+  canvas.style.width = "100%";
+  canvas.style.maxWidth = `${width * scale}px`;
+  // canvas.style.height = `${height * scale}px`;
   ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
   return canvas;
 };
@@ -26,6 +27,7 @@ export const setupCanvas = (id, width, height, scale) => {
   const elem = document.getElementById(id);
   resizeCanvas(elem, width, height, scale);
   const ctx = elem.getContext("2d");
+  elem.style.imageRendering = "pixelated";
   ctx.imageSmoothingEnabled = false;
   return ctx;
 };
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CELL_SIZE = 16;
 export const WIDTH = 8;
 export const HEIGHT = 10;
-export const SCALE = 3;
+export const SCALE = 3.5;
